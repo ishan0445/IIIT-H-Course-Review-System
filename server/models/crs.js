02bd8561de3362a2a3675075579b332f5af7ae49
Jayant Tanwani <Btanwani77@gmail.com>
2017-07-15 17:15:12 +0530

    Update crs.js

--- server/models/crs.js
+++ server/models/crs.js
@@ -1,11 +1,40 @@
 var mongoose = require('mongoose');
+/*
+"profName":"Vikram Pudi",
+			"profDesignation":"Associate Professor",
+			"profQualification":"Ph.D. (IISc Bangalore)",
+			"researchAreas":"Data mining, artificial intelligence, database systems",
+			"lab":"CDE , COS"
+
+*/
 var newProf = mongoose.model('faculty',{
   profName: {
     type: String,
     required: true,
     minlength:1,
     trim: true
-  }
+  },
+  profDesignation: {
+    type: String,
+    trim: true,
+    default: 'Not Available'
+  },
+  profQualification: {
+    type: String,
+    default: 'Not Available',
+    trim: true
+  },
+  researchAreas: {
+    type: String,
+    default: 'Not Available',
+    trim: true
+  },
+  lab: {
+    type: String,
+    default: 'Not Available',
+    trim: true
+  },
+
 });
 var newcourse = mongoose.model('courses',{
   courseID: {
