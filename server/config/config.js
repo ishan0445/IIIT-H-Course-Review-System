15afa0d8c6fca3f8dd7d0ccc831767ab893beac5
Jayant Tanwani <Btanwani77@gmail.com>
2017-07-15 16:25:11 +0530

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -8,15 +8,3 @@ if (env === 'development' || env === 'test') {
     process.env[key] = envConfig[key];
   });
 }
-
-
-// if(env === 'development') {
-//   process.env.PORT = 3000;
-//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-//   // process.env.MONGODB_URI = 'mongodb://Jayant:Jayant@ds149501.mlab.com:49501/todoapp';
-// } else if(env === 'test') {
-//   process.env.PORT = 4000;
-//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-//
-//
-// }
